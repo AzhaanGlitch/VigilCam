ceb94d416db9a4bcd68d46779303d9da697e73be
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-09 03:56:08 +0530

    db js added

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/vigilcam';
+    
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    
+    console.log('MongoDB Connected Successfully');
+  } catch (err) {
+    console.error('MongoDB Connection Error:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
