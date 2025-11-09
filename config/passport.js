b52a715d74ab95a6a4a7ef8e4d4743c9a1506e49
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-09 19:34:16 +0530

    passport js updated

--- config/passport.js
+++ config/passport.js
@@ -40,7 +40,9 @@ module.exports = function(passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/users/auth/google/callback',
+        callbackURL: process.env.NODE_ENV === 'production' 
+          ? 'https://vigilcam.onrender.com/users/auth/google/callback'
+          : '/users/auth/google/callback',
         proxy: true
       },
       async (accessToken, refreshToken, profile, done) => {
